2ef9fa805978db6d9dacac7c925282dba3b1bf4b
Pauline Whang <pauline.s.wh@gmail.com>
2017-05-20 00:06:00 -0400

    first commit friendfinder

---
+++ App/data/friends.js
@@ -0,0 +1,40 @@
+var friends = [
+ { 
+ name: 'Andrew',
+ photo: '',
+ scores: [5, 5, 5, 3, 3, 3, 2, 2, 2, 1]
+ },
+
+ { 
+ name: 'Matt',
+ photo: '',
+ scores: [4, 4, 4, 3, 3, 3, 2, 2, 2, 1]
+ },
+
+ { 
+ name: 'Tyler',
+ photo: '',
+ scores: [4, 5, 4, 3, 3, 6, 2, 3, 2, 1]
+ },
+
+ { 
+ name: 'Steven',
+ photo: '',
+ scores: [6, 7, 8, 3, 3, 3, 1, 1, 1, 1]
+ },
+
+ { 
+ name: 'John',
+ photo: '',
+ scores: [4, 7, 3, 1, 2, 3, 2, 2, 1, 1]
+ },
+
+ { 
+ name: 'Jason',
+ photo: '',
+ scores: [4, 1, 2, 3, 6, 3, 2, 2, 2, 1]
+ },
+
+ ];
+
+ module.exports = friends;
